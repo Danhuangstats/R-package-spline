552fb62ffe001477480d651b3354f627661a6fb1
Danniehuang0405 <33643613+Danniehuang0405@users.noreply.github.com>
2021-06-28 16:45:34 -0400

    Add files via upload

---
+++ R/des.stats.R
@@ -0,0 +1,27 @@
+
+#' Create histogram, boxplot and numeric summary
+#' @export
+#' @param x numeric variable
+
+
+
+ddes.stats=function(x){
+  ### 1 row and 2 colmns
+  par(mfrow=c(1,2))
+  ## histogram
+  hist(x,col=rainbow(30))
+
+  ## boxplot
+
+  boxplot(x,col = 'green')
+  par(mfrow=c(1,1))
+
+  ## numeric summary
+
+  data.frame(min=min(x),
+             median=median(x),
+             mean=mean(x),
+             max=max(x))
+
+
+}
